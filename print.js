4d4d11e4a0ffe5a00858b492dd10ee8d1d9975ec
areswoo <asdfwoo93@gmail.com>
2022-05-12 13:59:45 -0500

    [Test]Test

---
+++ print.js
@@ -0,0 +1,12 @@
+function printPage(){
+    var initPage = document.body.innerHTML;
+    function beforePrint(){
+        document.body.innerHTML = document.getElementsByTagName("main")[0].innerHTML;
+    }
+    function afterPrint(){
+        document.body.innerHTML = initPage;
+    }
+    window.onbeforeprint = beforePrint;
+    window.print();
+    window.onafterprint = afterPrint;
+}
