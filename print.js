9a5747e3169f54b33283179a86cc3117863da116
areswoo <asdfwoo93@gmail.com>
2022-05-12 23:28:12 -0500

    [Update]Update

--- print.js
+++ print.js
@@ -1,12 +1,22 @@
 function printMain(){
     const initPage = document.body.innerHTML;
+    const faV4 = document.getElementById("fa-v4-font-face");
+    const faV5 = document.getElementById("fa-v5-font-face");
+    const faV4Shim = document.getElementById("fa-v4-shims");
+    const faMain = document.getElementById("fa-main");
+    var sizeValue = document.getElementById("print_size");
+    var value = sizeValue.options[sizeValue.selectedIndex].value;
+
+
     function beforePrint(){
-        document.body.innerHTML = document.getElementsByTagName("main")[0].outerHTML;
+        document.body.innerHTML = document.getElementsByTagName("main")[0].outerHTML + faV5.outerHTML + faV4.outerHTML + faV4Shim.outerHTML + faMain.outerHTML;
+        document.getElementsByTagName("main")[0].classList.add(value);
+        console.log(faV5.outerHTML);
     }
-    function afterPrint(original){
-        document.body.innerHTML = original;
+    function afterPrint(){
+        document.body.innerHTML = initPage;
     }
     window.onbeforeprint = beforePrint;
     window.print();
-    window.onafterprint = afterPrint(initPage);
+    window.onafterprint = afterPrint();
 }
