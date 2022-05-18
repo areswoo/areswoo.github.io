3730ca69d416175989bbeac95c8c4b5885018e24
areswoo <asdfwoo93@gmail.com>
2022-05-18 05:47:10 -0500

    [Test]Test

--- print.js
+++ print.js
@@ -1,17 +1,23 @@
-function printMain(){
+function printMain() {
     const initPage = document.body.innerHTML;
     var sizeValue = document.getElementById("print_size");
     var value = sizeValue.options[sizeValue.selectedIndex].value;
 
 
-    function beforePrint(){
+    function beforePrint() {
         document.body.innerHTML = document.getElementsByTagName("main")[0].outerHTML;
         document.getElementsByTagName("main")[0].classList.add(value);
     }
-    function afterPrint(){
+    function afterPrint() {
         document.body.innerHTML = initPage;
     }
-    window.addEventListener('beforeprint',beforePrint);
+    function beforePrintHandler() {
+        for (let id in Chart.instances) {
+            Chart.instances[id].resize();
+        }
+    }
+    window.addEventListener('beforeprint', beforePrint);
+    window.addEventListener('beforeprint', beforePrintHandler);
     window.print();
-    window.addEventListener('afterprint',afterPrint());
+    window.addEventListener('afterprint', afterPrint());
 }
